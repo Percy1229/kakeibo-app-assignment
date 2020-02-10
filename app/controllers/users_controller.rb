688a9cadb2d9f31d7a303c1386d6c97d88e02fd2
percy1998 <percy3227@yahoo.co.jp>
2020-02-10 05:20:43 +0000

    debugged user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   end
 
   def show
+     @user = User.find(params[:id])
   end
 
   def new
@@ -23,7 +24,8 @@ class UsersController < ApplicationController
   
   private 
   
-  def user_params
-    params.require(:user).permit(:name, :email, :passoword, :passoword_confirmation)
+  def user_params 
+    params.require(:user).permit(
+      :name, :email, :password, :password_confirmation)
   end
 end
