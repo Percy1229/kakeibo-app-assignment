dffdbb8876730eab30a1f781e48fe9e514c616c5
percy1998 <percy3227@yahoo.co.jp>
2020-02-10 11:14:42 +0000

    edit validate for Eglish only

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,9 @@
 class List < ApplicationRecord
   belongs_to :user
+    #数字のみを許可
     validates :expense, presence: true, numericality: true
-    validates :place, presence: true, length: { maximum: 255 }
-    validates :item_name, presence: true, length: { maximum: 255 }
+    
+    #英語・数字のみの入力を許可する
+    validates :place, presence: true, length: { maximum: 255 }, format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true }
+    validates :item_name, presence: true, length: { maximum: 255 }, format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true }
 end
