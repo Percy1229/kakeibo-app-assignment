4106c5a42f402e10094bd89fd142f45df2ff4f70
percy1998 <percy3227@yahoo.co.jp>
2020-02-11 17:45:43 +0000

    add error messages & modify Regex

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,6 +4,6 @@ class List < ApplicationRecord
     validates :expense, presence: true, numericality: true
     
     #英語・数字のみの入力を許可する
-    validates :place, presence: true, length: { maximum: 255 }, format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true }
-    validates :item_name, presence: true, length: { maximum: 255 }, format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true }
+    validates :place, presence: true, length: { maximum: 255 }, format: { with: /\A[A-Za-z0-9\s]*\z/, allow_blank: true }
+    validates :item_name, presence: true, length: { maximum: 255 }, format: { with: /\A[A-Za-z0-9\s]*\z/, allow_blank: true }
 end
