6a28ed8234c37da5761bb4f5a9cb238ab27438e0
percy1998 <percy3227@yahoo.co.jp>
2020-02-12 04:37:10 +0000

    add category to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_035519) do
+ActiveRecord::Schema.define(version: 2020_02_12_043136) do
 
   create_table "incomes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "income"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_02_12_035519) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
