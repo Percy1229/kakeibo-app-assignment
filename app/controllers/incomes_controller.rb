6c156ca938cd1fe479cf463466132e8cb61627df
percy1998 <percy3227@yahoo.co.jp>
2020-02-15 13:12:57 +0000

    try heroku error

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -8,7 +8,7 @@ class IncomesController < ApplicationController
       @income = current_user.incomes
       
       #dateカラムで検索する
-      @incomes = @income.where('date::text LIKE ?', "%#{params[:date]}%").order("date DESC").page(params[:page]).per(10)
+      @incomes = @income.where('date.to_s LIKE ?', "%#{params[:date]}%").order("date DESC").page(params[:page]).per(10)
 
       
       @income_total = 0
